1140435736a7c95b752fd0c70e8913bf61eea541
jtrecenti <jtrecenti@jtrecenti.com>
2017-02-15 10:53:05 +0000

    Update curso-r

--- img/banners/rescale-images.R
+++ img/banners/rescale-images.R
@@ -7,3 +7,11 @@ laptop <- image_read('static/img/banners/laptop-original.jpg')
 image_scale(laptop, '1200') %>% 
   image_crop('1000x750+200') %>% 
   image_write('static/img/banners/laptop.jpg')
+
+# ler a imagem original
+bookshop <- image_read('static/img/banners/bookshop-original.jpg')
+
+# dar um rescale p/ ficar com o tamanho de 1000 por 750
+bookshop %>% 
+  image_crop('1000x750') %>% 
+  image_write('static/img/banners/bookshop.jpg')
